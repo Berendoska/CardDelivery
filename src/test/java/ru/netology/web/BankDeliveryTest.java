a89f9843f7b83b0ff06914128df348a53f204246
kate <k-b-v@yandex.ru>
2022-11-01 18:06:14 +0300

    correct

--- src/test/java/ru/netology/web/BankDeliveryTest.java
+++ src/test/java/ru/netology/web/BankDeliveryTest.java
@@ -6,6 +6,7 @@ import com.codeborne.selenide.WebDriverRunner;
 //import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+ import org.junit.jupiter.api.BeforeEach;
 
 import java.time.Duration;
 
@@ -13,12 +14,21 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
+
+
 public class BankDeliveryTest {
+
+    @BeforeEach
+    void setUpTest () {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+    }
 
 
     @Test
     void test() {
-        open("http://localhost:9999");
+
+
         $("[class='input__control']").setValue("Магас");
         $("[value='04.11.2022']").setValue("04.11.2022");
        // $("[.//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input']");
