6965dad34f90cc40174fc785ab2dfd0255b3f6af
kate <k-b-v@yandex.ru>
2022-11-14 19:52:44 +0300

    Change Code and add LocateDate

--- src/test/java/ru/netology/web/BankDeliveryTest.java
+++ src/test/java/ru/netology/web/BankDeliveryTest.java
@@ -3,23 +3,28 @@ package ru.netology.web;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
-//import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
- import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
-
 public class BankDeliveryTest {
 
+
+    private String generateDate(int i) {
+        return LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @BeforeEach
-    void setUpTest () {
+    void setUpTest() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
     }
@@ -30,16 +35,16 @@ public class BankDeliveryTest {
 
 
         $("[class='input__control']").setValue("Магас");
-        $("[value='04.11.2022']").setValue("04.11.2022");
-       // $("[.//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input']");
-        //while (By.xpath())
+        String planningDate = generateDate(4);
+        //$("[value='dd.MM.yyyy']").setValue("dd.MM.yyyy");
         $("[name='name']").setValue("Ирова-Иркина Ирина");
         $("[name='phone']").setValue("+79888888888");
         $x("//span[@class = 'checkbox__text']").click();
         $$x("//span[@class = 'button__text']").last().click();
         $("//*[contains(text(),'Встреча успешно забронирована на')]");
-
-
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
 
     }
---
+++ src/test/java/ru/netology/web/DataPlusDays.java
@@ -0,0 +1,11 @@
+package ru.netology.web;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+public class DataPlusDays {
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+}
